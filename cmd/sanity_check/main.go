57551f925840efa36eb94c2218f6f27d3d92b549
Florent Biville <florent.biville@neo4j.com>
2022-10-14 17:05:56 +0200

    Improve sanity check, force generics usage
    That way, it can only run with Go 1.18+

--- cmd/sanity_check/main.go
+++ cmd/sanity_check/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println(hello("world"))
+}
+
+// use generics to make sure Go 1.18+ is installed
+func hello[T string](value T) string {
+	return fmt.Sprintf("Hello %s!", value)
 }
