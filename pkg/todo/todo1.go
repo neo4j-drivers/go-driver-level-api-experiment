c727bd452f0ba1fa2063a7c0b9689cf7ee75b50f
Florent Biville <florent.biville@neo4j.com>
2022-10-14 17:12:17 +0200

    Split TODO in different files, reuse across main functions

---
+++ pkg/todo/todo1.go
@@ -0,0 +1,11 @@
+package todo
+
+import "github.com/neo4j-drivers/go-driver-level-api-experiment/pkg/neo4j_alpha"
+
+// Todo1 :
+//   - use the above driver and run the Cypher query "RETURN 42"
+//   - call panicInTheErr on the error, if the API you call returns one
+//   - print the result with fmt.Println
+func Todo1(driver neo4j_alpha.Driver) {
+	// YOUR CODE GOES HERE
+}
